7b368aa41aaf0815693360369195352a0dcc6d0b
josecanterovilla <josevc@byui.edu>
2025-01-14 16:10:31 -0300

    a

---
+++ hola.js
@@ -0,0 +1,24 @@
+myName = "Chris";
+
+function logName() {
+  console.log(myName);
+}
+
+logName();
+
+var myName;
+
+let myjob = "peter"
+let myjob = "peter"
+// error
+
+let dog = { name: "Spot", breed: "Dalmatian" };
+
+
+dog.name;
+
+console.log(42 / +0); // Infinity
+console.log(42 / -0);
+
+
+

7d6d53c874371dc42925fdc4411416730a31ec97
josecanterovilla <josevc@byui.edu>
2025-02-02 03:29:57 -0300

    a

---
+++ scripts/local.js
@@ -0,0 +1,16 @@
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+
+function formatDate(date) {
+    const options = {
+        month: '2-digit',
+        day: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true,  
+    };
+    
+    return date.toLocaleString('en-US', options);  
+}

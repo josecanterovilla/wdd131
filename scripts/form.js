03e75523299573cde79cd0d51d3fb7b247b1195a
josecanterovilla <josevc@byui.edu>
2025-02-20 15:11:47 -0300

    a

--- scripts/form.js
+++ scripts/form.js
@@ -23,3 +23,24 @@ const products = [
     localStorage.setItem("reviewCount", reviewCount);
     document.getElementById("reviewCount").textContent = reviewCount;
   }
+
+
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+
+function formatDate(date) {
+    const options = {
+        month: '2-digit',
+        day: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true,  
+    };
+    
+    return date.toLocaleString('en-US', options);  
+}
+
+
+document.getElementById('lastModified').textContent = formatDate(new Date(document.lastModified));

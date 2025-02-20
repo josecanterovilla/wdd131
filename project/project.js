d16a061e98d4211df4b66f76041afab71780ce59
josecanterovilla <josevc@byui.edu>
2025-02-20 20:26:58 -0300

    a

--- project/project.js
+++ project/project.js
@@ -78,3 +78,14 @@ document.getElementById("contactForm").addEventListener("submit", function(event
         confirmationMessage.classList.remove("hidden");
     });
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu-icon");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuIcon.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+});
